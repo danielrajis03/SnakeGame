c91f20e9e829f9f20225b6b06467382d1ab5406e
psydr5 <psydr5@nottingham.ac.uk>
2022-11-30 15:17:56 +0000

    added label and text area for name entry

---
+++ src/main/java/controller/Scene02controller.java
@@ -0,0 +1,33 @@
+package controller;
+import example.Main;
+import com.example.snake.model.Data;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
+/*public class Scene02controller {
+    @FXML
+    private AnchorPane scene02;
+
+    @FXML
+    private TextArea txaWelcome;
+
+    @FXML
+    void sayBye(ActionEvent event) throws IOException {
+        AnchorPane scene01= FXMLLoader.load(Main.class.getResource("fxml/scene01.fxml"));
+        scene02.getChildren().removeAll();
+        scene02.getChildren().setAll(scene01);
+    }
+
+    @FXML
+    void initialize() {
+        txaWelcome.setText("Welcome "+Data.text);
+    }
+}*/
+
+
