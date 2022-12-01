92b7cb055d7d3eea9e193a5dfdc78459edf8605e
psydr5 <psydr5@nottingham.ac.uk>
2022-12-01 01:05:51 +0000

    attempt 1 to get from scene 1 to scene 2

--- src/main/java/controller/Scene01controller.java
+++ src/main/java/controller/Scene01controller.java
@@ -1,58 +1,40 @@
 package controller;
-
-
-
 import GamePlay.Play;
+import example.Main;
+import com.example.snake.model.Data;
 import example.MusicPlayer;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
 
-    public class Scene01controller {
-
+public class Scene01controller {
     @FXML
     private AnchorPane scene01;
 
+    @FXML
+    private TextField txtName;
 
-
-        /*
-            @FXML
-            private TextField txtName;
-        */
     @FXML
     void moveToNextScene(ActionEvent event) throws IOException {
-
+        Data.text = txtName.getText();
+        AnchorPane scene02 = FXMLLoader.load(Main.class.getClass().getResource("fxml/scene02.fxml"));
         scene01.getChildren().removeAll();
-
-    }
-
-    @FXML
-    void startGame(ActionEvent event) {
-        System.out.println("Game Started");
-        new Play().loadFrame();
-        MusicPlayer.getMusicPlay("src/main/resources/frogger.mp3");
-        scene01.getChildren().removeAll();
-
-
-
-
-
-
-
-
+        scene01.getChildren().setAll(scene02);
     }
 
     @FXML
     void exitGame(ActionEvent event) {
-        System.out.println("Game Over");
+        System.out.println("Bye!");
         System.exit(0);
-
     }
-/*
-        @FXML
-        void initialize() {
-            txtName.setText(Data.text);
-        }*/
+
+    @FXML
+    void initialize() {
+        txtName.setText(Data.text);
+    }
 }
--- src/main/java/controller/Scene02controller.java
+++ src/main/java/controller/Scene02controller.java
@@ -1,33 +1,51 @@
 package controller;
+
+import GamePlay.Play;
 import example.Main;
 import com.example.snake.model.Data;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
-
-/*public class Scene02controller {
+public class Scene02controller {
+    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/scene02.fxml"));
+    Scene scene = new Scene(fxmlLoader.load());
     @FXML
     private AnchorPane scene02;
 
     @FXML
+    private AnchorPane scene01;
+
+    @FXML
     private TextArea txaWelcome;
+
+    public Scene02controller() throws IOException {
+    }
 
     @FXML
     void sayBye(ActionEvent event) throws IOException {
-        AnchorPane scene01= FXMLLoader.load(Main.class.getResource("fxml/scene01.fxml"));
+        AnchorPane scene01 = FXMLLoader.load(Main.class.getClass().getResource("fxml/scene01.fxml"));
         scene02.getChildren().removeAll();
         scene02.getChildren().setAll(scene01);
     }
 
     @FXML
+    void startGame(ActionEvent event) {
+        System.out.println("Game Started");
+        new Play().loadFrame();
+        System.exit(0);
+
+        scene01.getChildren().removeAll();
+    }
+    @FXML
     void initialize() {
         txaWelcome.setText("Welcome "+Data.text);
     }
-}*/
+}
 
 
