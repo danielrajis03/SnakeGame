655cecd9696d387a4c41ada1d34b0ebd97ea1e2a
psydr5 <psydr5@nottingham.ac.uk>
2022-12-08 23:17:51 +0000

    Junit tests

---
+++ src/main/java/test/JavaTests/JavaTests/MyFrameTest.java
@@ -0,0 +1,30 @@
+package JavaTests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyFrameTest {
+
+    @Test
+    void name() {
+    }
+
+
+    @Test
+    void loadFrame() {
+
+    }
+
+    @Test
+    void keyTyped() {
+    }
+
+    @Test
+    void keyPressed() {
+    }
+
+    @Test
+    void keyReleased() {
+    }
+}
